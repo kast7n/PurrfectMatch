d4353c8bf55ecf6b6c8987db11b2e96cfcd5f49d
Ali Rida Darsa <Milotheapache@gmail.com>
2024-04-19 19:10:14 +0300

    Implementation of search bar

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask,render_template,redirect
+from flask import Flask,render_template,redirect,request
 
 app = Flask(__name__,template_folder='templates')
 
 
 
-@app.route("/")
+@app.route("/", methods = ["GET","POST"])
 def homePage():
+    if request.method == "POST":
+        searchURL = request.form["searchPet"]
+        return redirect(f"/search/{searchURL}")
     return render_template("index.html")
 
 @app.route("/search/<name>")
