4b7c493127485eab10bb082272faf82b7039a365
Ali Rida Darsa <Milotheapache@gmail.com>
2024-04-20 17:26:20 +0300

    further fixes on search page changed svg's folder name to svg

--- app.py
+++ app.py
@@ -8,11 +8,11 @@ app = Flask(__name__,template_folder='templates')
 def homePage():
     if request.method == "POST":
         searchURL = request.form["searchPet"]
-        return redirect(f"/search/{searchURL}")
+        return redirect(f"/search/All/{searchURL}")
     return render_template("index.html")
 
-@app.route("/search/<name>")
-def search(name):
-    return render_template("searchResults.html",petName = name)
+@app.route("/search/<type>/<name>")
+def search(type,name):
+    return render_template("searchResults.html",petType = type,petName = name)
 
 app.run(debug=True)
