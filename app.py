46b29c524b8010c4296835adb9ed156c25e6ab9d
Ali Rida Darsa <Milotheapache@gmail.com>
2024-05-08 10:04:49 +0300

    minor fixes on search

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
+
 from flask import Flask,render_template,redirect,request
 import sqlalchemy
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import or_,and_
+
 
 app = Flask(__name__,template_folder='templates')
 
-Base = declarative_base()
+Base = sqlalchemy.orm.declarative_base()
 
 class Pet(Base):
     __tablename__ = 'pet'
@@ -21,24 +24,19 @@ Session = sessionmaker(bind=engine)
 def homePage():
     if request.method == "POST":
         searchURL = request.form["searchPet"]
-        return redirect(f"/search/All/{searchURL}")
+        return redirect(f"/search/all/{searchURL}")
     return render_template("index.html")
 
 @app.route("/search/<type>/<name>")
 def search(type,name):
     pets = []
     session = Session()
-    if type == "all":    
-        if name == "all":
-            pets = session.query(Pet).all()
-        else:
-            pets = session.query(Pet).filter(Pet.petName == name).all()
+    if type == "all":
+        condition = Pet.petName == name if name != "all" else True
     else:
-    
-        if name == "all":
-            pets = session.query(Pet).filter(Pet.petType == type).all()
-        else:
-            pets = session.query(Pet).filter(Pet.petName == name, Pet.petType == type).all()
+        condition = Pet.petType == type if name == "all" else and_(Pet.petName == name, Pet.petType == type)
+
+    pets = session.query(Pet).filter(condition).all()
     
     session.close()
 
